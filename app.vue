28114a5c3c8639291bf3e1478a7499069a9af0e4
keenwang <keenwang@zego.im>
2022-04-10 13:40:16 +0800

    🎉 git init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- <div>
+    <NuxtWelcome />
+  </div> -->
+  <router-view></router-view>
+</template>
---
+++ layouts/custom.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>这个自定义布局</h1>
+    <slot />
+  </div>
+</template>
