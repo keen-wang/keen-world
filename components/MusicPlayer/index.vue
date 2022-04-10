2fb38540dd9a9044459d9072d679a011b58aaff1
keenwang <keenwang@zego.im>
2022-04-11 01:28:53 +0800

    ✨ 新增显示歌曲信息栏

--- components/MusicPlayer/index.vue
+++ components/MusicPlayer/index.vue
@@ -1,46 +1,31 @@
 <template>
   <div class="player-wrap">
     <div class="bg_player_mask"></div>
-    <div class="bg_player" style=""></div>
+    <div
+      class="bg_player"
+      :style="{
+        'background-image': `url(
+         ${data.activeMusic.poster}
+        )`,
+      }"
+    ></div>
     <div class="mod_player">
       <div class="player__bd">
         <div id="song_info__info" class="bd-left">
-          <a
-            class="song_info__cover"
-            href="/n/ryqq/albumDetail/001whii43nuvNe"
-            target="_blank"
-            rel="noopener noreferrer"
-            ><img
-              class="song_info__pic"
-              loading="lazy"
-              src="//y.qq.com/music/photo_new/T002R300x300M000001whii43nuvNe_1.jpg?max_age=2592000"
-              data-qar-def="//y.qq.com/mediastyle/global/img/album_300.png?max_age=2592000"
-              alt="晚风心里吹"
-          /></a>
+          <img
+            class="song_info__pic"
+            loading="lazy"
+            :src="data.activeMusic.poster"
+            data-qar-def="//y.qq.com/mediastyle/global/img/album_300.png?max_age=2592000"
+            alt="晚风心里吹"
+          />
           <div class="song_info__name">
-            歌曲名：<a
-              href="/n/ryqq/songDetail/003N7qbb2oHqUk?"
-              target="_blank"
-              rel="noopener noreferrer"
-              >晚风心里吹</a
-            >
+            歌曲名： {{ data.activeMusic.songName }}
           </div>
           <div class="song_info__singer">
-            歌手：<a
-              class="playlist__author"
-              title="阿梨粤"
-              href="/n/ryqq/singer/002ffRcI0XN8px"
-              >阿梨粤</a
-            >
+            歌手： {{ data.activeMusic.singer }}
           </div>
-          <div class="song_info__album">
-            专辑：<a
-              href="/n/ryqq/albumDetail/001whii43nuvNe"
-              target="_blank"
-              rel="noopener noreferrer"
-              >晚风心里吹</a
-            >
-          </div>
+          <div class="song_info__album">专辑：{{ data.activeMusic.album }}</div>
         </div>
         <div id="song_info__lyric" class="bd-right"></div>
       </div>
@@ -54,7 +39,7 @@
 </template>
 <script setup>
 import { onMounted, watch } from "vue";
-import mp3Wenfeng from "./music/问风-Keen.mp3";
+import musicList from "./musicList";
 const props = defineProps({
   id: {
     type: String,
@@ -72,7 +57,7 @@ watch(
   }
 );
 onMounted(() => {
-  document.title = "音乐播放器";
+  document.title = "Music Time";
   try {
     initPlayer();
   } catch (error) {
@@ -81,26 +66,14 @@ onMounted(() => {
 });
 const { $xgplayer } = useNuxtApp();
 let player = undefined;
-const musicList = reactive([
-  {
-    src: mp3Wenfeng,
-    name: "Keen · 问风",
-    poster:
-      "//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/poster-small.jpeg",
-    lyric: `[00:00.00] 脆弱一分钟\n`,
-  },
-  {
-    src: "//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/audio.mp3",
-    name: "林宥嘉·脆弱一分钟",
-    poster:
-      "//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/poster-small.jpeg",
-    lyric: `[00:00.00] 脆弱一分钟\n[00:00.00] 作曲 : 林家谦\n[00:00.00] 作词 : 徐世珍/吴辉福\n[00:00.000]编曲：林家谦\n[00:00.000]时钟不要走\n[00:04.220]让我脆弱一分钟\n[00:07.440]要多久才能习惯被放手\n[00:15.800]马克杯空了 暖暖的温热\n[00:22.660]却还在我手中停留\n[00:27.960]\n[00:29.790]勇气不要走\n[00:32.200]给我理由再冲动\n[00:35.690]去相信爱情 就算还在痛\n[00:43.960]如果我不说不会有人懂\n[00:50.720]失去你我有多寂寞\n[00:55.610]还是愿意\n[00:57.580]付出一切仅仅为了一个好梦\n[01:03.980]梦里有人真心爱我 陪我快乐也陪我沉默\n[01:11.260]没有无缘无故的痛承受越多越成熟\n[01:18.630]能让你拥抱更好的我\n[01:25.030] 谁也不要走\n[01:28.270]应该是一种奢求\n[01:31.900]可是我只想 握紧你的手\n[01:39.780]我宁愿等候 也不愿错过\n[01:46.630]你对我微笑的时候\n[01:56.780]\n[02:18.910]还是愿意\n[02:21.320]用尽全力仅仅为了一个以后\n[02:27.870]哪怕生命并不温柔哪怕被幸福一再反驳\n[02:34.870]也要相信伤痕累累 其实只是在琢磨\n[02:42.070]能让你为之一亮 的我\n[02:53.910]\n[02:56.350]制作人：林宥嘉\n[02:57.750]制作助理：张婕汝\n[02:59.010]录音师：陈文骏、叶育轩\n[03:00.410]录音室：白金录音室\n[03:01.740]混音师：SimonLi @ nOiz\n[03:03.000]OP: Terence Lam Production & Co. (Warner/Chappell Music, HK Ltd.)\n[03:04.050]SP: Warner/Chappell Music Taiwan Ltd.\n[03:04.910]OP：Universal Ms Publ Ltd Taiwan\n`,
-  },
-]);
+const data = reactive({
+  list: musicList,
+  activeMusic: musicList[0],
+});
 const initPlayer = () => {
   player = new $xgplayer({
     id: props.id,
-    url: musicList,
+    url: data.list,
     volume: 0.5,
     volumeShow: true,
     width: "100%",
@@ -114,16 +87,25 @@ const initPlayer = () => {
   //   }
   // });
 
-  player.lyric(musicList[0].lyric, document.querySelector("#song_info__lyric"));
+  player.lyric(
+    data.activeMusic.lyric,
+    document.querySelector("#song_info__lyric")
+  );
   player.__lyric__.show();
   // var an = player.analyze(document.querySelector("canvas"));
   // an.style = {
   //   bgColor: "#c8c8c8",
   //   color: "#909099",
   // };
-  player.on("change", function ({ name }) {
-    const music = musicList.find((item) => item.name === name);
-    player.lyric(music.lyric, document.querySelector("#song_info__lyric"));
+  player.on("change", ({ name }) => {
+    player.__lyric__.unbind(player);
+    document.querySelector("#song_info__lyric").innerHTML = "";
+    data.activeMusic = musicList.find((item) => item.name === name);
+    player.lyric(
+      data.activeMusic.lyric,
+      document.querySelector("#song_info__lyric")
+    );
+    document.title = data.activeMusic.name + " - Music Time";
     player.__lyric__.show();
   });
 };
@@ -156,7 +138,6 @@ const initPlayer = () => {
   .bg_player {
     // 设置背景
     display: block;
-    background-image: url(//y.qq.com/music/photo_new/T002R300x300M000000Hg3fW1GN9NF.jpg?max_age=2592000);
     background-color: rgb(255, 255, 255);
     background-repeat: no-repeat;
     background-size: cover;
@@ -188,6 +169,27 @@ const initPlayer = () => {
       #song_info__info {
         display: flex;
         flex-direction: column;
+        text-align: center;
+        font-size: 14px;
+        line-height: 24px;
+        color: hsla(0, 0%, 88.2%, 0.8);
+        align-items: center;
+        justify-content: center;
+        .song_info__album,
+        .song_info__name,
+        .song_info__singer {
+          white-space: nowrap;
+          width: 100%;
+          text-overflow: ellipsis;
+          height: 28px;
+        }
+        .song_info__name {
+          margin-top: 15px;
+        }
+        .song_info__pic {
+          height: 60%;
+          margin: 0 auto;
+        }
       }
       #song_info__lyric {
         -webkit-mask-image: linear-gradient(
