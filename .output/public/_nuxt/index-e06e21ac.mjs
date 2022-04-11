33f0bfb01d75ec39f7a3f1b129a45b75b5aa8e2f
keenwang <keenwang@zego.im>
2022-04-11 13:01:52 +0800

    🚀  打包静态页面

---
+++ .output/public/_nuxt/index-e06e21ac.mjs
@@ -0,0 +1,118 @@
+import{m,_ as r,w as c,o as A,u as p,r as y,a as _,c as l,b as E,n as g,t as D,d as f,e as B,f as v,g as h}from"./entry-c0a34551.mjs";const C=[{src:m,name:"Keen \xB7 \u95EE\u98CE",poster:"http://p1.music.126.net/M1gk3yEHg5TnPlj1-w4Mzw==/109951166466504110.jpg?param=177y177",lyric:`
+[ti:\u95EE\u98CE]
+[ar:GAI\u5468\u5EF6,\u5409\u514B\u96BD\u9038]
+[al:\u5929\u8D50\u7684\u58F0\u97F3\u7B2C\u4E09\u5B63 \u7B2C1\u671F]
+[by:p_pttzhang]
+[offset:0]
+[00:00.00]\u95EE\u98CE (Live) - Keen
+[00:02.49]\u8BCD\uFF1A\u674E\u4F26\u5E8F
+[00:02.62]\u66F2\uFF1A\u674E\u4F26\u5E8F
+[00:02.88]\u6539\u7F16\u8BCD\uFF1AGAI\u5468\u5EF6/\u798F\u7B11
+[00:03.81]\u6539\u7F16\u66F2\uFF1AGAI \u5468\u5EF6/SOOVA/\u7C73\u6B23Mixin/\u798F\u7B11
+[00:05.00]\u7F16\u66F2\uFF1A\u4F59\u5A01
+[00:05.12]\u539F\u5531\uFF1A\u91D1\u6E14
+[00:15.18]\u601D\u5FF5\u5316\u6210\u98CE \u5212\u7834\u4E86\u957F\u7A7A
+[00:17.78]\u95EF\u8FDB\u6211\u7684\u68A6\u91CC
+[00:21.96]\u805A\u6563\u7684\u4E89\u8BAE \u79BB\u5408\u7684\u5B9A\u4E49
+[00:24.68]\u90A3\u672A\u89E3\u4E4B\u8C1C
+[00:28.88]\u4F60\u51ED\u4EC0\u4E48\u4E0D\u7ED9\u4E00\u4E2A\u7406\u7531
+[00:32.23]\u7136\u540E\u628A\u6211\u629B\u5F03
+[00:35.53]\u4F60\u7684\u706B\u773C\u91D1\u775B\u770B\u4E0D\u51FA\u6211\u773C\u91CC\u7684\u72B9\u8C6B
+[00:42.20]\u8FD8\u4E0D\u80FD\u591F\u4ECE\u4F60\u7684\u68A6\u91CC\u82CF\u9192
+[00:45.41]\u6211\u8FD8\u6CA1\u6709\u7F1D\u8865\u53D7\u4F24\u7684\u8EAF\u4F53
+[00:48.96]\u8FEB\u4E0D\u53CA\u5F85 \u95EE\u4F60 \u95EE\u4F60
+[00:51.67]\u662F\u5426\u7559\u4E0B\u6211\u7684\u8DB3\u8FF9
+[00:56.09]\u8FD8\u6765\u4E0D\u53CA\u95EE\u98CE\u5E26\u6211\u53BB\u54EA\u91CC
+[00:59.36]\u8D8A\u8FC7\u4E86\u6208\u58C1\u8FD8\u662F\u6DF1\u57CB\u8C37\u5E95
+[01:02.74]\u6211\u6709\u4E00\u4E2A \u95EE\u9898 \u95EE\u9898
+[01:05.51]\u54EA\u91CC\u53EF\u4EE5\u627E\u5230\u4F60
+[01:24.69]\u601D\u5FF5\u5316\u6210\u98CE \u5212\u7834\u4E86\u957F\u7A7A
+[01:27.33]\u95EF\u8FDB\u6211\u7684\u68A6\u91CC
+[01:31.64]\u805A\u6563\u7684\u4E89\u8BAE \u79BB\u5408\u7684\u5B9A\u4E49
+[01:34.23]\u90A3\u672A\u89E3\u4E4B\u8C1C
+[01:38.42]\u4F60\u51ED\u4EC0\u4E48\u4E0D\u7ED9\u4E00\u4E2A\u7406\u7531
+[01:41.62]\u7136\u540E\u628A\u6211\u629B\u5F03
+[01:45.06]\u4F60\u7684\u706B\u773C\u91D1\u775B\u770B\u4E0D\u51FA\u6211\u773C\u91CC\u7684\u72B9\u8C6B
+[01:52.81]\u6CA1\u6709\u4F60\u5728 \u6211\u8FD8\u4F1A\u662F\u6211\u5417
+[01:54.47]\u4EBA\u751F\u8BE5 \u5F80\u53F3\u8FD8\u662F\u5DE6\u5462
+[01:56.15]\u65F6\u95F4\u5C3D\u5934\u53EA\u5269\u4E0B\u7070\u70EC
+[01:57.64]\u7075\u9B42\u6DF1\u5904\u4E5F\u4E0D\u4F1A\u518D\u6709\u706B\u82B1
+[01:59.66]\u4F60\u6211\u4E4B\u95F4\u7559\u4E0B\u4E86\u4EC0\u4E48
+[02:01.29]\u56DE\u5FC6\u5B9A\u683C \u7070\u8272\u8FD8\u6709\u5417
+[02:03.02]\u4F60\u7684\u79BB\u5F00 \u638F\u7A7A\u4E86\u5168\u90E8
+[02:04.45]\u53EA\u5269\u4E0B\u8EAF\u58F3 \u4E00\u5207\u90FD\u88AB\u62B9\u6740
+[02:06.64]\u4F60\u8D50\u7684\u7F8E\u9152 \u5176\u5B9E\u91CC\u9762\u90FD\u662F\u6BD2
+[02:09.38]\u5012\u4E0D\u5982 \u65E9\u6B8A\u9014
+[02:10.82]\u653E\u4E00\u628A\u706B\u70E7\u4E86\u8FD9\u5E45\u56FE
+[02:12.91]\u5403\u4E86\u6211\u5403\u4E0D\u4E86\u7684\u82E6
+[02:14.58]\u4EAB\u4E86\u6211\u4EAB\u4E0D\u4E86\u7684\u798F
+[02:16.37]\u60F3\u95EE\u98CE \u4F55\u5904\u9A7B\u8DB3
+[02:18.07]\u98CE\u5374\u5439\u7184\u6211\u7684\u70DB
+[02:19.49]\u8FD8\u4E0D\u80FD\u591F\u4ECE\u4F60\u7684\u68A6\u91CC\u82CF\u9192
+[02:22.67]\u6211\u8FD8\u6CA1\u6709\u7F1D\u8865\u53D7\u4F24\u7684\u8EAF\u4F53
+[02:26.18]\u8FEB\u4E0D\u53CA\u5F85 \u95EE\u4F60 \u95EE\u4F60
+[02:28.93]\u662F\u5426\u7559\u4E0B\u6211\u7684\u8DB3\u8FF9
+[02:33.32]\u8FD8\u6765\u4E0D\u53CA\u95EE\u98CE\u5E26\u6211\u53BB\u54EA\u91CC
+[02:36.63]\u8D8A\u8FC7\u4E86\u6208\u58C1\u8FD8\u662F\u6DF1\u57CB\u8C37\u5E95
+[02:40.14]\u6211\u6709\u4E00\u4E2A \u95EE\u9898 \u95EE\u9898
+[02:42.80]\u54EA\u91CC\u53EF\u4EE5\u627E\u5230\u4F60
+[02:47.31]\u85CF\u6709\u592A\u591A\u95EE\u9898 \u8A00\u8BED\u592A\u950B\u5229
+[02:50.68]\u4E5F\u6765\u4E0D\u53CA\u95EE\u4F60 \u4E3A\u4EC0\u4E48\u79BB\u53BB
+[02:54.16]\u5C31\u50CF\u8131\u7F51\u7684\u98DE\u9C7C
+[02:56.42]\u8DCC\u843D\u8FDB\u4E86\u5E9F\u589F
+[02:58.12]\u8C01\u4E5F\u4E0D\u60F3\u4E3A\u654C
+[02:59.88]\u6DF1\u6D77\u4E0D\u662F\u552F\u4E00
+[03:01.45]\u4F60\u603B\u5728\u597D\u5947 \u90A3\u6211\u8FD9\u6B21\u544A\u8BC9\u4F60
+[03:05.02]\u6211\u672C\u7802\u783E\u6C89\u4E8E\u6DF1\u6D77\u968F\u98CE\u800C\u53BB
+[03:08.28]\u53EF\u6211\u5E76\u6CA1\u5728\u610F \u7802\u783E\u5212\u4F24\u81EA\u5DF1
+[03:11.50]\u52AB\u540E\u91CD\u751F\u7518\u613F\u6210\u4E3A\u5171\u540C\u4F53
+[03:16.72]\u8FD8\u4E0D\u80FD\u591F\u4ECE\u4F60\u7684\u68A6\u91CC\u82CF\u9192
+[03:20.13]\u6211\u8FD8\u6CA1\u6709\u7F1D\u8865\u53D7\u4F24\u7684\u8EAF\u4F53
+[03:23.66]\u8FEB\u4E0D\u53CA\u5F85 \u95EE\u4F60 \u95EE\u4F60
+[03:26.36]\u662F\u5426\u7559\u4E0B\u6211\u7684\u8DB3\u8FF9
+[03:30.73]\u8FD8\u6765\u4E0D\u53CA\u95EE\u98CE\u5E26\u6211\u53BB\u54EA\u91CC
+[03:34.05]\u8D8A\u8FC7\u4E86\u6208\u58C1\u8FD8\u662F\u6DF1\u57CB\u8C37\u5E95
+[03:37.52]\u6211\u6709\u4E00\u4E2A \u95EE\u9898 \u95EE\u9898
+[03:40.26]\u54EA\u91CC\u53EF\u4EE5\u627E\u5230\u4F60`,songName:"\u95EE\u98CE",singer:"Keen",album:"\u95EE\u98CE"},{src:"//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/audio.mp3",name:"\u6797\u5BA5\u5609\xB7\u8106\u5F31\u4E00\u5206\u949F",poster:"//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/poster-small.jpeg",lyric:`[00:00.00] \u8106\u5F31\u4E00\u5206\u949F
+[00:00.00] \u4F5C\u66F2 : \u6797\u5BB6\u8C26
+[00:00.00] \u4F5C\u8BCD : \u5F90\u4E16\u73CD/\u5434\u8F89\u798F
+[00:00.000]\u7F16\u66F2\uFF1A\u6797\u5BB6\u8C26
+[00:00.000]\u65F6\u949F\u4E0D\u8981\u8D70
+[00:04.220]\u8BA9\u6211\u8106\u5F31\u4E00\u5206\u949F
+[00:07.440]\u8981\u591A\u4E45\u624D\u80FD\u4E60\u60EF\u88AB\u653E\u624B
+[00:15.800]\u9A6C\u514B\u676F\u7A7A\u4E86 \u6696\u6696\u7684\u6E29\u70ED
+[00:22.660]\u5374\u8FD8\u5728\u6211\u624B\u4E2D\u505C\u7559
+[00:27.960]
+[00:29.790]\u52C7\u6C14\u4E0D\u8981\u8D70
+[00:32.200]\u7ED9\u6211\u7406\u7531\u518D\u51B2\u52A8
+[00:35.690]\u53BB\u76F8\u4FE1\u7231\u60C5 \u5C31\u7B97\u8FD8\u5728\u75DB
+[00:43.960]\u5982\u679C\u6211\u4E0D\u8BF4\u4E0D\u4F1A\u6709\u4EBA\u61C2
+[00:50.720]\u5931\u53BB\u4F60\u6211\u6709\u591A\u5BC2\u5BDE
+[00:55.610]\u8FD8\u662F\u613F\u610F
+[00:57.580]\u4ED8\u51FA\u4E00\u5207\u4EC5\u4EC5\u4E3A\u4E86\u4E00\u4E2A\u597D\u68A6
+[01:03.980]\u68A6\u91CC\u6709\u4EBA\u771F\u5FC3\u7231\u6211 \u966A\u6211\u5FEB\u4E50\u4E5F\u966A\u6211\u6C89\u9ED8
+[01:11.260]\u6CA1\u6709\u65E0\u7F18\u65E0\u6545\u7684\u75DB\u627F\u53D7\u8D8A\u591A\u8D8A\u6210\u719F
+[01:18.630]\u80FD\u8BA9\u4F60\u62E5\u62B1\u66F4\u597D\u7684\u6211
+[01:25.030] \u8C01\u4E5F\u4E0D\u8981\u8D70
+[01:28.270]\u5E94\u8BE5\u662F\u4E00\u79CD\u5962\u6C42
+[01:31.900]\u53EF\u662F\u6211\u53EA\u60F3 \u63E1\u7D27\u4F60\u7684\u624B
+[01:39.780]\u6211\u5B81\u613F\u7B49\u5019 \u4E5F\u4E0D\u613F\u9519\u8FC7
+[01:46.630]\u4F60\u5BF9\u6211\u5FAE\u7B11\u7684\u65F6\u5019
+[01:56.780]
+[02:18.910]\u8FD8\u662F\u613F\u610F
+[02:21.320]\u7528\u5C3D\u5168\u529B\u4EC5\u4EC5\u4E3A\u4E86\u4E00\u4E2A\u4EE5\u540E
+[02:27.870]\u54EA\u6015\u751F\u547D\u5E76\u4E0D\u6E29\u67D4\u54EA\u6015\u88AB\u5E78\u798F\u4E00\u518D\u53CD\u9A73
+[02:34.870]\u4E5F\u8981\u76F8\u4FE1\u4F24\u75D5\u7D2F\u7D2F \u5176\u5B9E\u53EA\u662F\u5728\u7422\u78E8
+[02:42.070]\u80FD\u8BA9\u4F60\u4E3A\u4E4B\u4E00\u4EAE \u7684\u6211
+[02:53.910]
+[02:56.350]\u5236\u4F5C\u4EBA\uFF1A\u6797\u5BA5\u5609
+[02:57.750]\u5236\u4F5C\u52A9\u7406\uFF1A\u5F20\u5A55\u6C5D
+[02:59.010]\u5F55\u97F3\u5E08\uFF1A\u9648\u6587\u9A8F\u3001\u53F6\u80B2\u8F69
+[03:00.410]\u5F55\u97F3\u5BA4\uFF1A\u767D\u91D1\u5F55\u97F3\u5BA4
+[03:01.740]\u6DF7\u97F3\u5E08\uFF1ASimonLi @ nOiz
+[03:03.000]OP: Terence Lam Production & Co. (Warner/Chappell Music, HK Ltd.)
+[03:04.050]SP: Warner/Chappell Music Taiwan Ltd.
+[03:04.910]OP\uFF1AUniversal Ms Publ Ltd Taiwan
+`,songName:"\u8106\u5F31\u4E00\u5206\u949F",singer:"\u6797\u5BA5\u5609",album:"\u8106\u5F31\u4E00\u5206\u949F"}],b={props:{id:{type:String,required:!0,default:"music-player"}},setup(a,{expose:o}){o();const s=a;c(()=>s.videoUrl,n=>{i()},{deep:!0}),A(()=>{document.title="Music Time";try{i()}catch(n){console.error(n.toString())}});const{$xgplayer:F}=p();let u;const e=y({list:C,activeMusic:C[0]}),i=()=>{u=new F({id:s.id,url:e.list,volume:.5,volumeShow:!0,width:"100%"}),u.crossOrigin="anonymous",u.lyric(e.activeMusic.lyric,document.querySelector("#song_info__lyric")),u.__lyric__.show(),u.on("change",({name:n})=>{u.__lyric__.unbind(u),document.querySelector("#song_info__lyric").innerHTML="",e.activeMusic=C.find(d=>d.name===n),u.lyric(e.activeMusic.lyric,document.querySelector("#song_info__lyric")),document.title=e.activeMusic.name+" - Music Time",u.__lyric__.show()})},t={props:s,$xgplayer:F,player:u,data:e,initPlayer:i,onMounted:A,watch:c,musicList:C};return Object.defineProperty(t,"__isScriptSetup",{enumerable:!1,value:!0}),t}},M={class:"player-wrap"},x=E("div",{class:"bg_player_mask"},null,-1),w={class:"mod_player"},S={class:"player__bd"},P={id:"song_info__info",class:"bd-left"},L=["src"],O={class:"song_info__name"},q={class:"song_info__singer"},T={class:"song_info__album"},$=E("div",{id:"song_info__lyric",class:"bd-right"},null,-1),j=["id"],k=E("div",{id:"mask"},null,-1);function N(a,o,s,F,u,e){return _(),l("div",M,[x,E("div",{class:"bg_player",style:g({"background-image":`url(
+         ${F.data.activeMusic.poster}
+        )`})},null,4),E("div",w,[E("div",S,[E("div",P,[E("img",{class:"song_info__pic",loading:"lazy",src:F.data.activeMusic.poster,"data-qar-def":"//y.qq.com/mediastyle/global/img/album_300.png?max_age=2592000",alt:"\u665A\u98CE\u5FC3\u91CC\u5439"},null,8,L),E("div",O," \u6B4C\u66F2\u540D\uFF1A "+D(F.data.activeMusic.songName),1),E("div",q," \u6B4C\u624B\uFF1A "+D(F.data.activeMusic.singer),1),E("div",T,"\u4E13\u8F91\uFF1A"+D(F.data.activeMusic.album),1)]),$]),E("div",{class:"player__ft",id:s.id},null,8,j)]),k])}var z=r(b,[["render",N]]);const I=f({setup(){}}),K={class:"music-wrapper"};function G(a,o,s,F,u,e){const i=z,t=h;return _(),l("div",K,[B(t,null,{default:v(()=>[B(i,{id:"music-player"})]),_:1})])}var U=r(I,[["render",G],["__scopeId","data-v-d4b87cce"]]);export{U as default};
