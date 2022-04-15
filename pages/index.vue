ad1a3e823093d0b5f10aee53a448f97611edd9a4
keenwang <keenwang@zego.im>
2022-04-15 23:31:11 +0800

    🎉  git init nuxt2

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtLink to="/page2">{{ aa }}</NuxtLink>
+    <Tutorial />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+   name: "IndexPage",
+  data() {
+    return {
+      aa: "啊啊啊啊啊",
+    };
+  },
+});
+</script>
---
+++ pages/page2.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>这是页面2</h2>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
